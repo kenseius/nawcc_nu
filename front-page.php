5e5549754b48b12da9c698da863c7e16d1f79039
Brian Roy <kenseius@users.noreply.github.com>
2019-05-23 13:49:12 -0400

    intial fix to frontpage
    a more elegant solution exists to replace outdated php. however, the if(has_post_thumbnail) returns undesired  results.

--- front-page.php
+++ front-page.php
@@ -52,21 +52,10 @@
 <main>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php
-    // This code aquires the ID of the the posts's thumbnail, in order to extract its url
-    $thumbnail = '';
-    // Get the ID of the post_thumbnail (if it exists)
-    $post_image_id = get_post_thumbnail_id($post_to_use->ID);
-        // if it exists
-        if ($post_image_id) {
-            $thumbnail = wp_get_attachment_image_src( $post_image_id, 'full', false);
-            if ($thumbnail) (string)$thumbnail = $thumbnail[0];}
-            if (!empty($thumbnail)) {
-?>
-<?php } else { ?><?php } ?>
     
-    <!-- Featured Post -->
-    <article class="post twoCol" style="background-image:url(<?php if ($thumbnail) { echo $thumbnail ;} ?>)">
+<?php if (has_post_thumbnail ( $post->ID ) ): ?>
+   <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id ( $post->ID ), 'single-post-thumbnail'); ?>
+    <article class="post twoCol" style="background-image:url('<?php echo $image[0]; ?>')">
         <div>   
             <p class="subtitle"><?php $the_category = the_category(' '); if ($the_category) { echo '<span class="pipe">|</span>' . $the_category . '';} ?></p>
             <h2><?php the_title(); ?></h2>    
@@ -81,7 +70,9 @@
         </div>
         <?php } ?>
     </article>
-
+<?php endif; ?>
+    
+ 
 <?php endwhile; else: ?>
   <p><?php _e('Sorry, there are no posts.'); ?></p>
 <?php endif; ?>
