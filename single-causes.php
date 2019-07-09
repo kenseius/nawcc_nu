98ab93fe23479b378f5b5cdb39914efcafa8df44
Brian Roy <kenseius@users.noreply.github.com>
2019-07-09 16:01:02 -0400

    updates, styles, content for Causes (now called Funds)

--- single-causes.php
+++ single-causes.php
@@ -12,14 +12,62 @@
 
 <?php get_header(); ?>
 
+<main class="events">
+    
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <section class="post_content">
+    <?php
+    
+    // Load values and assing defaults.
+    $logo             = get_field( 'post_logo' ) ?: get_template_directory_uri() . '/partials/blockTemplates/img/placeholder_logo.png';
+    $icon             = get_field( 'post_icon' );
+    $background_image = get_field( 'background_image', $post->ID ) ?: get_template_directory_uri() . '/partials/blockTemplates/img/placeholder_bg.png';
+    $background_color = get_field( 'background_color' ) ?: '#fafafa';
+    
+    $image = wp_get_attachment_image_src( get_post_thumbnail_id ( $post->ID ), 'single-post-thumbnail'); 
+
+    ?>
+
+    <header class="hero wideTitle">
+        
+        <section         
+            class="hero wideTitle hero_event
+                <?php if($background_color): ?> hero_color <?php endif; ?> 
+                <?php if (has_post_thumbnail () ): ?> hero_image <?php endif; ?>"
+            style="
+                <?php if($background_color): ?>background-color:<?php echo $background_color; ?>;<?php endif; ?>        
+                <?php if (has_post_thumbnail () ): ?> background-image:url('<?php echo $image[0]; ?><?php else: ?><?php echo $background_image; ?>');<?php endif; ?>"
+        >
+            
+            <?php if ( $logo ) { ?>
+                <div>
+                    <p class="subtitle" style="color:#fafafa;">Causes</p>
+                    <h1 style="color:#fafafa !important;"><?php the_title(); ?></h1> 
+                    <a class="button" href="#">Donate</a>
+                </div>
+            <?php } elseif ( $icon ) { ?> 
+                <div>
+                    <?php echo $icon; ?>
+                </div>
+            <?php } ?>
+            
+        </section>
+
+    </header>
+    
+    <section class="article_title event_title">
+        <p class="subtitle">Events</p>
+        <h1><?php the_title(); ?></h1> 
+    </section>
+    <section class="post_content wide_content nospaceCovers event_content">
         <?php the_content(); ?>
     </section>
 
+</main>
+
 <?php endwhile; else: ?>
-  <p><?php _e('Sorry, there are no posts. - single.php'); ?></p>
+  <p><?php _e('Sorry, there are no posts.'); ?></p>
 <?php endif; ?>
+
 
 <?php get_footer(); ?>
