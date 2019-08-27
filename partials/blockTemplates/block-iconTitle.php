0b169a4d03f11ef089c328e2e0acf3320a09b2a4
Brian Roy <kenseius@users.noreply.github.com>
2019-08-27 15:20:45 -0400

    added: acf field for icon Title's subtitle, blockOutlined styles; fixed layout styles

--- partials/blockTemplates/block-iconTitle.php
+++ partials/blockTemplates/block-iconTitle.php
@@ -9,6 +9,7 @@
 <?php
 	$title_icon		= get_field('title_icon');
 	$title_text		= get_field('title_text');
+	$title_subText	= get_field('title_subText');
 	$heading_level	= get_field('heading_level');
 ?>
 
@@ -20,6 +21,10 @@
 
 	<?php if( $title_text ): ?>
 		<span><?php echo $title_text; ?></span>
+	<?php endif; ?>
+
+	<?php if( $title_subText ): ?>
+		<span class="title_subText"><?php echo $title_subText; ?></span>
 	<?php endif; ?>
 
 </<?php if($heading_level): ?><?php echo $heading_level; ?><?php endif; ?>>
